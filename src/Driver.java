991235540974dfb436e7eec6c24b28d3f5d926fb
h-reyes <reyes.haley23@gmail.com>
2024-12-09 10:01:34 -0500

    Imports

--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,6 @@
+import java.io.IOException;
 public class Driver{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         
     }
---
+++ src/PolygonType.java
@@ -0,0 +1,25 @@
+// imports
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.BorderLayout;
+import java.io.File;
+
+
+public class PolygonType {
+    //members
+    private int numSides;
+    private String name;
+
+
+// method for getting numbers to image
+
+
+
+
+
+
+
+}
